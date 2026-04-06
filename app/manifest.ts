394efe56959e051c4049ef34c59bbac8381e75ab
Aneel Gautam <aneelgautam22@gmail.com>
2026-04-06 16:58:29 +0545

    add root manifest for pwa fix

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,13 +2,15 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Restaurant App",
-    short_name: "Restaurant App",
+    name: "Restrofy", // 👈 generic name राख
+    short_name: "Restrofy",
     description: "Smart Restaurant Management",
+
     start_url: "/",
     display: "standalone",
     background_color: "#020617",
     theme_color: "#020617",
+
     icons: [
       {
         src: "/icon-192.png",
@@ -18,11 +20,6 @@ export default function manifest(): MetadataRoute.Manifest {
       {
         src: "/icon-512.png",
         sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "/apple-icon.png",
-        sizes: "180x180",
         type: "image/png",
       },
     ],
